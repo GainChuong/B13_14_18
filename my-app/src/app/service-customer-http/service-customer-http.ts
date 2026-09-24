24f229f607e87bc4f29865cad2b91e7e9016ddb7
giangnntk24411-eng <giangnntk24411@st.uel.edu.vn>
2026-09-24 14:35:12 +0700

    Sửa bài 18

--- my-app/src/app/service-customer-http/service-customer-http.ts
+++ my-app/src/app/service-customer-http/service-customer-http.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { CustomerHttpService } from '../services/customer-http-service';
 
 @Component({
@@ -8,10 +8,11 @@ import { CustomerHttpService } from '../services/customer-http-service';
   templateUrl: './service-customer-http.html',
 })
 export class ServiceCustomerHttp {
-  customers:any; 
-constructor(private _service: CustomerHttpService){ 
+  customers:any[] = [];
+constructor(private _service: CustomerHttpService, private _changeDetectorRef: ChangeDetectorRef){ 
 this._service.getCustomers().subscribe({ 
-next:(data)=>{this.customers=data}  
+next:(data)=>{this.customers=data; this._changeDetectorRef.detectChanges()},
+error:(error)=>{console.error('Không thể tải danh sách khách hàng:', error)}
 }) 
 } 
 }
--- my-app/src/app/services/customer-http-service.ts
+++ my-app/src/app/services/customer-http-service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CustomerHttpService {
-    private _url:string="./assets/data/customers.json"; 
+    private _url:string="/assets/data/customers.json"; 
 constructor(private _http: HttpClient) { } 
 getCustomers():Observable<ICustomer[]>{     
 return this._http.get<ICustomer[]>(this._url) 
